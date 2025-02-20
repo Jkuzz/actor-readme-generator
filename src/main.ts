ecfef2eda3380b783eec0b6eaa032d64ea7dbb33
Jan Kuzelik <jan.kuzelik@apify.com>
2025-02-20 11:17:03 +0100

    delete structured output code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { Actor, log } from 'apify';
 import { BeeAgent } from 'bee-agent-framework/agents/bee/agent';
 import { UnconstrainedMemory } from 'bee-agent-framework/memory/unconstrainedMemory';
-import { z } from 'zod';
 import { LangChainChatModel } from 'bee-agent-framework/adapters/langchain/backend/chat';
 import { ChatOpenAI } from '@langchain/openai';
-import { OpenAIChatModel } from 'bee-agent-framework/adapters/openai/backend/chat';
-import { CalculatorSumTool } from './tools/calculator.js';
-import { InstagramScrapeTool } from './tools/instagram.js';
-import { StructuredOutputGenerator } from './structured_response_generator.js';
 import { beeOutputTotalTokens, chargeForActorStart, chargeForModelTokens } from './ppe_utils.js';
 import { getActorData } from './actor_data.js';
 
@@ -18,9 +13,8 @@ interface Input {
     debug?: boolean;
 }
 
-const query = 'Generate a README.md for the following Apify actor:';
+const query = 'Generate a README for the following actor:';
 await Actor.init();
-
 /**
  * Actor code
 */
@@ -34,6 +28,9 @@ const {
 } = await Actor.getInput() as Input;
 if (debug) {
     log.setLevel(log.LEVELS.DEBUG);
+}
+if (!actorId) {
+    throw new Error('Actor ID is required.');
 }
 
 const actorData = await getActorData(actorId);
@@ -51,71 +48,29 @@ const llm = new LangChainChatModel(
 // The LangChain adapter does not work with the structured output generation
 // for some reason.
 // Create a separate LLM for structured output generation.
-const llmStructured = new OpenAIChatModel(modelName);
 const agent = new BeeAgent({
     llm,
     memory: new UnconstrainedMemory(),
-    tools: [new CalculatorSumTool(),
-    new InstagramScrapeTool()],
+    tools: [],
 });
-
-// Store tool messages for later structured output generation.
-// This can be removed if you don't need structured output.
-const structuredOutputGenerator = new StructuredOutputGenerator(llmStructured);
 
 // Prompt the agent with the query.
 // Debug log agent status updates, e.g., thoughts, tool calls, etc.
-const response = await agent
-    .run({ prompt: query })
-    .observe((emitter) => {
-        emitter.on('update', async ({ update }) => {
-            log.debug(`Agent (${update.key}) 🤖 : ${update.value}`);
-
-            // Save tool messages for later structured output generation.
-            // This can be removed if you don't need structured output.
-            if (['tool_name', 'tool_output', 'tool_input'].includes(update.key as string)) {
-                structuredOutputGenerator.processToolMessage(
-                    update.key as 'tool_name' | 'tool_output' | 'tool_input',
-                    update.value,
-                );
-            }
-            // End of tool message saving.
-        });
-    });
-
+const response = await agent.run({ prompt: query });
 const tokensTotal = beeOutputTotalTokens(response);
 await chargeForModelTokens(modelName, tokensTotal);
 
 log.info(`Agent 🤖 : ${response.result.text}`);
 
-// Hacky way to get the structured output.
-// Using the stored tool messages and the user query to create a structured output.
-const structuredResponse = await structuredOutputGenerator.generateStructuredOutput(query,
-    z.object({
-        totalLikes: z.number(),
-        totalComments: z.number(),
-        mostPopularPosts: z.array(z.object({
-            url: z.string(),
-            likes: z.number(),
-            comments: z.number(),
-            timestamp: z.string(),
-            caption: z.string().nullable().optional(),
-            alt: z.string().nullable().optional(),
-        })),
-    }));
-log.debug(`Structured response: ${JSON.stringify(structuredResponse)}`);
 // Since the token usage tracking does not work with the Bee LLM, we will
 // just charge the same amount of tokens as the total tokens used by the agent for the
 // structured output generation - which is mostly the tool calls passed to the structured output generator.
 await chargeForModelTokens(modelName, tokensTotal);
-// End of structured output generation.
 
 // Push results to the dataset.
 await Actor.pushData({
     query,
     response: response.result.text,
-    // This can be removed if you don't need structured output.
-    structuredResponse: structuredResponse.object,
 });
 log.info('Pushed the data into the dataset!');
 
